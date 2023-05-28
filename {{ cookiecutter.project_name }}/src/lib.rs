d35c10440e66f86e49db3c928f4659c5edfc621c
Lorenzo Panieri <panierilorenzo@gmail.com>
2023-05-29 00:44:37 +0200

    added iced gui template option

--- {{ cookiecutter.project_name }}/src/lib.rs
+++ {{ cookiecutter.project_name }}/src/lib.rs
@@ -1,5 +1,23 @@
-use nih_plug::prelude::*;
 use std::sync::Arc;
+{# unfortunately we need to explicitly check == "True" because Jinja is stringly typed #}
+{% if cookiecutter.__has_editor == "True" %}
+use atomic_float::AtomicF32;
+{% endif %}
+use nih_plug::prelude::*;
+{% if cookiecutter.editor_gui_framework == "egui" %}
+todo!();
+{% elif cookiecutter.editor_gui_framework == "iced" %}
+use nih_plug_iced::IcedState;
+{% elif cookiecutter.editor_gui_framework == "vizia" %}
+todo!();
+{% endif %}
+
+{% if cookiecutter.__has_editor == "True" %}
+mod editor;
+
+/// The time it takes for the peak meter to decay by 12 dB after switching to complete silence.
+const PEAK_METER_DECAY_MS: f64 = 150.0;
+{% endif %}
 
 // This is a shortened version of the gain example with most comments removed, check out
 // https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
@@ -7,10 +25,27 @@ use std::sync::Arc;
 
 struct {{ cookiecutter.struct_name }} {
     params: Arc<{{ cookiecutter.struct_name }}Params>,
+    {% if cookiecutter.__has_editor == "True" %}
+    /// Needed to normalize the peak meter's response based on the sample rate.
+    peak_meter_decay_weight: f32,
+    /// The current data for the peak meter. This is stored as an [`Arc`] so we can share it between
+    /// the GUI and the audio processing parts. If you have more state to share, then it's a good
+    /// idea to put all of that in a struct behind a single `Arc`.
+    ///
+    /// This is stored as voltage gain.
+    peak_meter: Arc<AtomicF32>,
+    {% endif %}
 }
 
 #[derive(Params)]
 struct {{ cookiecutter.struct_name }}Params {
+    {% if cookiecutter.editor_gui_framework == "iced" %}
+    /// The editor state, saved together with the parameter state so the custom scaling can be
+    /// restored.
+    #[persist = "editor-state"]
+    editor_state: Arc<IcedState>,
+    {% endif %}
+
     /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
     /// these IDs remain constant, you can rename and reorder these fields as you wish. The
     /// parameters are exposed to the host in the same order they were defined. In this case, this
@@ -23,6 +58,10 @@ impl Default for {{ cookiecutter.struct_name }} {
     fn default() -> Self {
         Self {
             params: Arc::new({{ cookiecutter.struct_name }}Params::default()),
+            {% if cookiecutter.__has_editor == "True" %}
+            peak_meter_decay_weight: 1.0,
+            peak_meter: Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+            {% endif %}
         }
     }
 }
@@ -30,6 +69,10 @@ impl Default for {{ cookiecutter.struct_name }} {
 impl Default for {{ cookiecutter.struct_name }}Params {
     fn default() -> Self {
         Self {
+            {% if cookiecutter.__has_editor == "True" %}
+            editor_state: editor::default_state(),
+            {% endif %}
+
             // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
             // to treat these kinds of parameters as if we were dealing with decibels. Storing this
             // as decibels is easier to work with, but requires a conversion for every sample.
@@ -99,15 +142,39 @@ impl Plugin for {{ cookiecutter.struct_name }} {
         self.params.clone()
     }
 
+    {% if cookiecutter.__has_editor == "True" %}
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        editor::create(
+            self.params.clone(),
+            self.peak_meter.clone(),
+            self.params.editor_state.clone(),
+        )
+    }
+    {% endif %}
+
     fn initialize(
         &mut self,
         _audio_io_layout: &AudioIOLayout,
+        {# deal with the leading underscore depending on whether the variable is unused #}
+        {% if cookiecutter.__has_editor == "True" %}
+        buffer_config: &BufferConfig,
+        {% else %}
         _buffer_config: &BufferConfig,
+        {% endif %}
         _context: &mut impl InitContext<Self>,
     ) -> bool {
         // Resize buffers and perform other potentially expensive initialization operations here.
         // The `reset()` function is always called right after this function. You can remove this
         // function if you do not need it.
+
+        {% if cookiecutter.__has_editor == "True" %}
+        // After `PEAK_METER_DECAY_MS` milliseconds of pure silence, the peak meter's value should
+        // have dropped by 12 dB
+        self.peak_meter_decay_weight = 0.25f64
+            .powf((buffer_config.sample_rate as f64 * PEAK_METER_DECAY_MS / 1000.0).recip())
+            as f32;
+        {% endif %}
+
         true
     }
 
@@ -123,12 +190,36 @@ impl Plugin for {{ cookiecutter.struct_name }} {
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
         for channel_samples in buffer.iter_samples() {
+            {% if cookiecutter.__has_editor == "True" %}
+            let mut amplitude = 0.0;
+            let num_samples = channel_samples.len();
+            {% endif %}
+
             // Smoothing is optionally built into the parameters themselves
             let gain = self.params.gain.smoothed.next();
-
             for sample in channel_samples {
                 *sample *= gain;
+                {% if cookiecutter.__has_editor == "True" %}
+                amplitude += *sample;
+                {% endif %}
             }
+            {% if cookiecutter.__has_editor == "True" %}
+            // To save resources, a plugin can (and probably should!) only perform expensive
+            // calculations that are only displayed on the GUI while the GUI is open
+            if self.params.editor_state.is_open() {
+                amplitude = (amplitude / num_samples as f32).abs();
+                let current_peak_meter = self.peak_meter.load(std::sync::atomic::Ordering::Relaxed);
+                let new_peak_meter = if amplitude > current_peak_meter {
+                    amplitude
+                } else {
+                    current_peak_meter * self.peak_meter_decay_weight
+                        + amplitude * (1.0 - self.peak_meter_decay_weight)
+                };
+
+                self.peak_meter
+                    .store(new_peak_meter, std::sync::atomic::Ordering::Relaxed)
+            }
+            {% endif %}
         }
 
         ProcessStatus::Normal
